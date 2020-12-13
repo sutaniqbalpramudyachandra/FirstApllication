935e26eabf22ca320acfae1d646adae043b82669
Sutan Iqbal Pramudya Chandra <71204100+sutaniqbalpramudyachandra@users.noreply.github.com>
2020-12-13 15:20:34 +0700

    Project 3

---
+++ app/src/main/java/com/sutaniqbalpc/firstapllication/ExplicitIntentActivity.kt
@@ -0,0 +1,29 @@
+package com.sutaniqbalpc.firstapllication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_explicit_intent.*
+
+class ExplicitIntentActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_EMAIL = "extra_email"
+        const val EXTRA_AGE = "extra_age"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explicit_intent)
+
+    txt_nama.text = intent.getStringExtra(EXTRA_NAME)
+        txt_email.text = intent.getStringExtra(EXTRA_EMAIL)
+        txt_age.text = intent.getStringExtra(EXTRA_AGE)
+
+    btn_back.setOnClickListener {
+        val backItem = Intent(this@ExplicitIntentActivity,MainActivity::class.java)
+        startActivity(backItem)
+    }
+
+    }
+}
--- app/src/main/java/com/sutaniqbalpc/firstapllication/MainActivity.kt
+++ app/src/main/java/com/sutaniqbalpc/firstapllication/MainActivity.kt
@@ -1,14 +1,60 @@
     package com.sutaniqbalpc.firstapllication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.sutaniqbalpc.firstapllication.model.User
+import kotlinx.android.synthetic.main.activity_main.*
 
     class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+        companion object {
+            const val REQUEST_CODE = 100
+        }
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.belajar_constraint_layout)
+        setContentView(R.layout.activity_main)
 
+        btn_simple_intent.setOnClickListener{
+            val simpleIntent = Intent(this@MainActivity,SimpleActivity::class.java)
+            startActivity(simpleIntent)
+        }
+
+        btn_intent_with_data.setOnClickListener {
+            val dataInten = Intent(this@MainActivity,ExplicitIntentActivity::class.java)
+            dataInten.putExtra(ExplicitIntentActivity.EXTRA_NAME,"Sepeda Brompton")
+            dataInten.putExtra(ExplicitIntentActivity.EXTRA_EMAIL,"1000")
+            dataInten.putExtra(ExplicitIntentActivity.EXTRA_AGE,"Rp.25.000.000")
+            startActivity(dataInten)
+        }
+        btn_intent_parcelable.setOnClickListener {
+            val parcelIntent = Intent(this@MainActivity,ParcleActivity::class.java)
+            val user = User("Sepeda Brompton","20 Desember 2020","Rp.25.000.000")
+            parcelIntent.putExtra(ParcleActivity.EXTRA_USER, user)
+            startActivity(parcelIntent)
+        }
+        btn_implicit_intent.setOnClickListener {
+            val phoneNumber = "089504497883"
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:$phoneNumber"))
+            intent.putExtra("sms_body","Hallo Pak")
+            startActivity(intent)
+
+        }
+
+        btn_intent_result.setOnClickListener {
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
     }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(resultCode, resultCode, data)
+            if(resultCode == 200){
+                val color = data?.getStringExtra(ResultActivity.EXTRA_COLOR)
+                Log.d("Color",color.toString())
+                view_result.setBackgroundColor(Color.parseColor(color.toString()))
+            }
+        }
 }
